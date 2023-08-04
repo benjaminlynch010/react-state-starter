c02a4268c7dbf34fe5907204ec90da4ae7e8f585
Ben <132373460+karuson@users.noreply.github.com>
2023-08-04 15:06:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React State Starter
+
+To be setup for leacture, run:
+```
+npm install
+npm start
+```
---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <p>Hello! My name is Luke.</p>
+            <button>Click me!</button>
+            <p>I've clicked the button 0 times.</p>
+        </div>
+    );
+}
+
+export default App;
